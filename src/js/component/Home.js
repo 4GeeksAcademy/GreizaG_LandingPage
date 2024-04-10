561478f315f92308452a491923a1720ec743bef5
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-04-10 01:00:01 +0000

    [Componente Navbar] Realicé código componente Navbar.js

---
+++ src/js/component/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+//create your first component
+const Home = () => {
+	return (
+		<div>
+			<Navbar />
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/js/component/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand text-light ms-3" href="#">Start Bootstrap</a>
+                <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-light" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled text-light" aria-disabled="true">Service</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled text-light me-3" aria-disabled="true">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
