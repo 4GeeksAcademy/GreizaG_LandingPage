db2bd1610075ea87f29b3d0cd92deca8255eae42
Greiza García <159574736+GreizaG@users.noreply.github.com>
2024-04-11 01:26:26 +0000

    [Agregué arreglo con info para componente Card] Generación y uso de props en el componente

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div class="col">
             <div class="card">
-                <img src="https://e00-especiales-marca.uecdn.es/motor/images/formula1/seo/seo-especial2023-mercedes.jpg" class="card-img-top" alt="Mercedes AMG F1" />
+                <img src={props.image} class="card-img-top" alt="Mercedes AMG F1" />
                 <div class="card-body pb-5 pt-4">
-                    <h4 class="text-center"><b>Mercedes AMG F1 Petronas</b></h4>
-                    <p class="card-text text-center my-3">Equipo de origen alemán con sedes en Brackley y Brixworth, en Gran Bretaña.</p>
+                    <h4 class="text-center"><b>{props.title}</b></h4>
+                    <p class="card-text text-center my-3">{props.description}</p>
                 </div>
                 <div class="d-flex card-footer justify-content-center">
                     <a href="#" class="btn btn-outline-info">Find Out More!</a>
--- src/js/component/Home.js
+++ src/js/component/Home.js
@@ -6,16 +6,21 @@ import Bottom from "./Bottom";
 
 //create your first component
 const Home = () => {
+	let info = [
+		{ image: "https://e00-especiales-marca.uecdn.es/motor/images/formula1/seo/seo-especial2023-mercedes.jpg", title: "Mercedes AMG F1 Petronas", description: "Equipo de origen alemán con sedes en Brackley y Brixworth, en Gran Bretaña." },
+		{ image: "https://www.record.com.mx/sites/default/files/styles/v2-crop768x433/public/articulos/2024/02/13/0f.jpg?itok=evjyO65x", title: "Scuderia Ferrari", description: "Es la escudería más longeva y el equipo en activo más antiguo de la categoría, el más exitoso y el que ha conseguido más victorias" },
+		{ image: "https://cdn-3.expansion.mx/dims4/default/27ae0f9/2147483647/strip/true/crop/1920x1080+0+0/resize/1200x675!/format/webp/quality/60/?url=https%3A%2F%2Fcdn-3.expansion.mx%2F0b%2F44%2F06ca40234deda789753aefcdf799%2Fred-bull-auto-temporada-2024.jpeg", title: "Red Bull Racing", description: "Es un equipo de origen austríaco con sede en Milton Keynes, en Gran Bretaña. Es propiedad de la marca de bebidas homónima." },
+		{ image: "https://www.f1aldia.com/images/articulos/39000/39676-n.jpg", title: "Aston Martin Aramco Cognizant Formula One Team", description: "Equipo de origen británico con sede en Silverstone, Gran Bretaña. Es propiedad de un consorcio de inversores encabezado por el multimillonario canadiense Lawrence Stroll." }]
 	return (
 		<div>
 			<Navbar />
 			<Jumbotron />
 			<div class="container-fluid px-5">
 				<div className="row">
-					<Card />
-					<Card />
-					<Card />
-					<Card />
+					{info.map((value, index) => {
+						return <Card key={index} image={value.image} title={value.title} description={value.description} />
+					})
+					}
 				</div>
 			</div>
 			<Bottom />
